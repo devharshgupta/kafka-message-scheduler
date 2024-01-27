1a23411559bf8acc081f4186d44bb5b889602493
dev-harshgupta <115594699+dev-harshgupta@users.noreply.github.com>
2024-01-28 00:08:38 +0530

    go init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/devharshgupta/kafka-message-scheduler
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,8 @@
+// main.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go project!")
+}
